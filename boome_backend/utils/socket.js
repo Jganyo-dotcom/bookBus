0ab95f05adc6581552fe3acce30442abfd35e556
Jganyo-dotcom <elikemejay@gmail.com>
2025-12-26 02:47:38 +0000

    adding cors

--- boome_backend/utils/socket.js
+++ boome_backend/utils/socket.js
@@ -2,7 +2,12 @@ let io;
 
 function init(server) {
   const { Server } = require("socket.io");
-  io = new Server(server, { cors: { origin: "*" } });
+  io = new Server(server, {
+    cors: {
+      origin: ["http://localhost:3000", "https://bookbus-2.onrender.com"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    },
+  });
 
   io.on("connection", (socket) => {
     console.log("Client connected:", socket.id);
